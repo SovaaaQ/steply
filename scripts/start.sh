552ae36286b4dd883a7a1b909e89d09a2cbe2092
SovaaaQ <toxic_mari@mail.ru>
2026-06-07 15:57:50 +0500

    fix

--- scripts/start.sh
+++ scripts/start.sh
@@ -186,7 +186,7 @@ fi
 configure_mobile_urls
 
 log "Building and starting Steply containers..."
-docker compose up --build -d -f docker-compose.prod.yml
+docker compose up --build -d
 
 wait_for_service postgres
 wait_for_service backend
